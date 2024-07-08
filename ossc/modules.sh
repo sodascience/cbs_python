ded5d4e76fd912b438c9762d1cc236c5466c005b
Flavio Hafner <hafner.flavio@gmail.com>
2024-07-08 14:14:06 +0200

    use torch from pip

--- ossc/modules.sh
+++ ossc/modules.sh
@@ -10,12 +10,10 @@ module load 2023
 module load Python/3.11.3-GCCcore-12.3.0
 module load SciPy-bundle/2023.07-gfbf-2023a
 module load scikit-learn/1.3.1-gfbf-2023a
-module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
+#module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
 module load LLVM/14.0.6-GCCcore-12.3.0-llvmlite
 module load tqdm/4.66.1-GCCcore-12.3.0
-#module load dask/2023.9.2-foss-2023a # this is the problem for matplotlib
 module load statsmodels/0.14.1-gfbf-2023a
-#module load scikit-image/0.22.0-foss-2023a
 module load aiohttp/3.8.5-GCCcore-12.3.0
 module load Graphviz/8.1.0-GCCcore-12.3.0 
 module load numba/0.58.1-foss-2023a
